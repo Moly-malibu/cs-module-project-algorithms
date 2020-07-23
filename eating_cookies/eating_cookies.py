56301ff3077892669860ed8f44462077e014dad2
Monica Bustamante <lilianalambda@gmail.com>
2020-07-23 09:55:13 -0700

    Project Algorithms Solution

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -20,3 +20,19 @@ if __name__ == "__main__":
     num_cookies = 5
 
     print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+
+#NAIVE
+
+def eating_cookies_naive(n):
+    if n < 0:
+        return 0
+    elif n == 0:
+         return 1
+    else:
+        return eating_cookies_naive(n-1) + eating_cookies_naive(n-2) + eating_cookies_naive(n-3) 
+
+if __name__ == "__main__":
+    # Use the main function here to test out your implementation
+    num_cookies = 5
+
+    print(f"There are {eating_cookies_naive(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
