9e104dabb8851da4c1c8dcd0fe222794612c5941
Monica Bustamante <lilianalambda@gmail.com>
2020-07-23 17:50:58 -0700

    Project Algorithms solutions

--- knapsack/knapsack.py
+++ knapsack/knapsack.py
@@ -1,18 +1,37 @@
 #!/usr/bin/python
 
-import sys
+#1)List comprehension -> initial cache
+#2)Loop for room optimization.
+#3)iterate range len items + 1.
+#4)Iterate range capacity +1.
+#5)first iterate value 0 select empty list.
+#6)size items i-1 > j.
+#7)conditions is or not met.
+#8)value 1 > 2.
+#9)set cache.
+#10)return cache. 
+
 from collections import namedtuple
 
 Item = namedtuple('Item', ['index', 'size', 'value'])
 
 def knapsack_solver(items, capacity):
-    if n == 0:
-          return 0
-        if (item[n-1]) > n):
-              
-
-    pass
-
+  cache = [[{} for k in range(capacity + 1)] for k in range(len(items) + 1)]
+  for i in range(len(items) + 1):
+    for j in range(capacity + 1):
+      if i == 0 or j == 0:
+        cache[i][j] = {'value': 0, 'select': []}
+      elif items[i-1].size > j:
+            cache[i][j] = cache[i-1][j]
+      else:
+          first_case = cache[i-1][j]
+          second_case = {'value': cache[i-1][j-items[i-1].size]['value'] + items[i-1].value, 
+                        'select': cache[i-1][j-items[i-1].size]['select'] + [items[i-1].index]}
+      if first_case['value'] > second_case['value']:
+          cache[i][j] = first_case
+      else:
+          cache[i][j] = second_case
+  return cache[len(items)][capacity]
 
 if __name__ == '__main__':
   if len(sys.argv) > 1:
